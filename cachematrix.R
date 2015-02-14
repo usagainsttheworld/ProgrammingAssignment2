815f080810f2863d9541ac181efe7f4a801c5fd1
Ling Tian <sweetbellor.ling@gmail.com>
2015-02-14 15:27:23 -0500

    1st draft

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation. So it may make sense to cache the inverse of a matrix reather than compute it repeatedly for futher use. The following two functions are performed to cache the inverse of a matrix.
 
-## Write a short comment describing this function
-
+## This function creates a special "matrix" that can cache its inverse. It returns a list of functions including: set the value of the matrix; get the value of the matrix; set the value of the inverse of the matrix, get the value of the inverse of the matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y){
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv<<-inverse
+        getinverse <- function() inv
+        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse the matrix. It will first check if the inverse has already been calculated. if so, the cachesolve will retrieve the inverse from the cache. If not, it will compute the inverse, and sets the value in the cache by using setinverse function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv<-x$getinverse()
+        if(!is.null(inv)){
+                message("getting cached inverse matrix")
+                return(inv)       
+        }
+        data<-x$get()
+        inv<-solve(data,...)
+        x$setinverse(inv)
+        inv## Return a matrix that is the inverse of 'x'
 }
+
+##Test
+m<-makeCacheMatrix(matrix(1:4,2,2))
+m$get()
+##No cache in the first run
+cacheSolve(m)
+##Retrieving from the cache in the second run (see the message"getting cached inverse matrix")
+cacheSolve(m)
+
+
+
